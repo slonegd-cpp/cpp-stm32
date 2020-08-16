53c42f0f5cba4dcaccd95f7f65a364172b278b2c
Kiryushin Denis <dkiryushin@sedmax.com>
2020-08-16 08:57:22 +0300

    simplify linker script

--- main.cpp
+++ main.cpp
@@ -1,38 +1,17 @@
-#include <cstdint> // uint32_t
-
-class Register {
-    using Pointer = volatile uint32_t*;
-    Pointer pointer;
-
-public:
-    Register (uint32_t base, int offset) 
-        : pointer { reinterpret_cast<Pointer>(base + offset) }
-    {}
-
-    void write (uint32_t v) { *pointer = v; }
-};
+#include <cstdint> // uintptr_t
 
 int main() {
-    auto constexpr rcc_base   = 0x4002'3800;
-    auto constexpr gpioi_base = 0x4002'2000;
-
-    auto ahb1enr =  Register (rcc_base, 0x30);
-    ahb1enr.write(1 << 8);
-
-    auto moder = Register (gpioi_base, 0);
-    moder.write(1 << (1 * 2));
-
-    auto bssr = Register (gpioi_base, 0x18);
-    bssr.write(1 << 1);
-
-    while(1) {}
+    volatile auto i{0};
+    while (1) {
+        i++;
+    }
 }
 
 struct startup {
-    uint32_t stack_pointer;
-    uint32_t entry_point;
+    uintptr_t stack_pointer;
+    uintptr_t entry_point;
 };
 __attribute__((section(".startup"))) startup _ {
     .stack_pointer = 0x20050000,
-    .entry_point = reinterpret_cast<uint32_t>(main)
+    .entry_point = reinterpret_cast<uintptr_t>(main)
 };
